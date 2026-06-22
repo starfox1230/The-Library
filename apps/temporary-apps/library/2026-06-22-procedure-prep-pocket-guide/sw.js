6f338311b30c35d322128e958476c796e6e10368
starfox1230 <134647937+starfox1230@users.noreply.github.com>
2026-06-21 23:47:10 -0500

    Add procedure prep pocket guide

---
+++ apps/temporary-apps/library/2026-06-22-procedure-prep-pocket-guide/sw.js
@@ -0,0 +1,26 @@
+const CACHE = "procedure-prep-pocket-guide-v2";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./app-icon.svg"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request, copy));
+      return response;
+    }))
+  );
+});
